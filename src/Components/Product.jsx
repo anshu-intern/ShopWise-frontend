a33fa39b09bfbde491325f35bdb5b119a24f8ffb
anshu-intern <anshumanik00@gmail.com>
2025-11-08 22:45:00 +0530

    Image size adjustment

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,10 +1,10 @@
 function Product({value}){
     return(
         <>
-        <div className="relative flex flex-col h-[400px] w-[280px] gap-2 border border-gray-400 rounded-xl overflow-hidden shadow-xl hover:scale-105 duration-300 cursor-pointer hover:border-blue-400 hover:border-3 hover:shadow-2xl">
+        <div className="relative flex flex-col h-[400px] w-[280px] gap-2 border border-gray-200 rounded-xl overflow-hidden shadow-xl hover:scale-105 duration-300 cursor-pointer hover:border-blue-400 hover:border-3 hover:shadow-2xl">
 
             <div className="relative w-[100%] h-[70%] overflow-hidden">
-                <img src={value.variant[0].imageUrl} alt={value.name} className="h-[100%] w-[100%]" />
+                <img src={value.variant[0].imageUrl} alt={value.name} className="h-[100%] w-[100%] p-14" />
             </div>
 
             <div className="flex flex-col justify-start items-start px-2 pb-2">
